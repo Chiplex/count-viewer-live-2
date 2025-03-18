e57f470ea0672b8d54ddaf33e128661f08ee3bf0
Alex Arce <raaz11chip@gmail.com>
2025-03-18 16:28:54 -0400

    Modificar configuración del servidor para permitir conexiones desde cualquier host y agregar imagen de Facebook

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ if __name__ == '__main__':
     update_thread.start()
     
     # Iniciar servidor con configuración modificada
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)  # agregado allow_unsafe_werkzeug=True
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
